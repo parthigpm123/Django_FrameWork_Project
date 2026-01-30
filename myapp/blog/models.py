eb3e09a639c0828f2f1ef05b3d2f7e72658b2146
parthigpm123 <parthigpm123@gmail.com.com>
2026-01-29 23:09:08 -0800

    insertdata

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
       title = models.CharField(max_length=100)
       content = models.TextField()
       img_url = models.URLField(null=True)
