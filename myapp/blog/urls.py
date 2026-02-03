ea2731e773d425cb1cd82da164247fcc01b783dc
parthigpm123 <parthigpm123@gmail.com.com>
2026-02-03 00:47:42 -0800

    commite

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
       
       #path('new_url', views.new_url, name='new_url'),
 
-      #path('old_url', views.old_url_redirect, name='old_url'),
+      path('contact', views.contact_view, name='contact'),
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,6 +5,7 @@ import logging
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator
+from .forms import ContactForm
 
 # Create your views here.
 #static demo data
@@ -50,5 +51,24 @@ def old_url_redirect(request, post_id):
 
 def new_url(request):
       return HttpResponse("This is the new URL page.")
+
+def contact_view(request):
+    if request.method == 'POST':
+      form = ContactForm(request.POST)
+      name = request.POST.get('name')
+      email = request.POST.get('email')
+      message = request.POST.get('message')
+       
+      logger = logging.getLogger('TESTING')
+      if form.is_valid():
+          logger.debug(f'POST data is {form.cleaned_data['name']} {form.cleaned_data['email']} {form.cleaned_data['message']}')
+          success_message = "Your email has been sent successfully!😎"
+          return render(request, 'contact.html', {'form': form, 'success_message':success_message})
+      else:
+            logger.debug(f'Form validation failure')
+      return render(request, 'contact.html', {'form': form, 'name': name, 'email': email, 'message': message})                  
+    return render(request, 'contact.html')
+
+      
 
 
